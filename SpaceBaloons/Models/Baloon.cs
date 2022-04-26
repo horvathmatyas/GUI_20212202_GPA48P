8d6c8519f982866121839d76524f449ccbfdc1a0
Joó Dávid Róbert <joo.david53@gmail.com>
2022-04-26 19:53:36 +0200

    created baloons model and methods

---
+++ SpaceBaloons/Models/Baloon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SpaceBaloons.Models
+{
+    internal class Baloon
+    {
+        public System.Drawing.Point Pos { get; set; }
+        public int Speed { get; set; }
+        public double Health { get; set; }
+        public bool InView { get; set; }
+        public Baloon(System.Drawing.Point pos, int speed, double health)
+        {
+            Pos = pos;
+            Speed = speed;
+            Health = health;
+        }       
+        public void Move(System.Drawing.Size size)
+        {
+            System.Drawing.Point newPos = new System.Drawing.Point(Pos.X, Pos.Y+Speed);
+            if (InView)
+            {
+                Pos = newPos;
+            }
+        }
+        
+        
+
+    }
+}
