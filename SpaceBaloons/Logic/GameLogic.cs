1b3afc2e3d8a2b5a1ee13ea99eb9c4dbaf9d1ff3
zolijoo <joozoltan9@gmail.com>
2022-04-28 18:32:32 +0200

    fixed baloon - laser, baloon - ship intersections in TimeStep method

--- SpaceBaloons/Logic/GameLogic.cs
+++ SpaceBaloons/Logic/GameLogic.cs
@@ -18,6 +18,8 @@ namespace SpaceBaloons.Logic
 
         static Random r = new Random();
         public event EventHandler Changed;
+        public event EventHandler GameOver;
+
         System.Windows.Size area;
         public enum Controls
         {
@@ -69,14 +71,36 @@ namespace SpaceBaloons.Logic
                 {
                     player.Health -= Baloons[i].Health;
                     Baloons.RemoveAt(i);
+                    if (player.Health > Baloons[i].Health)
+                    {
+                        player.Health -= Baloons[i].Health;
+                        Baloons.RemoveAt(i);
+                    }
+                    else
+                    {
+                        Baloons.RemoveAt(i);
+                        GameOver?.Invoke(this, null);
+
+                    }
                 }
                 for (int j = 0; j < Lasers.Count; j++)
                 {
                     Rect laserRect = new Rect(Lasers[i].Pos.X - 3, Lasers[i].Pos.Y - 5, 6, 10);
                     if (laserRect.IntersectsWith(baloonRect))
                     {
-                        Baloons.RemoveAt(i);
-                        Lasers.RemoveAt(j);
+
+                        if (Baloons[i].Health == 1)
+                        {
+                            Baloons.RemoveAt(i);
+                            Lasers.RemoveAt(j);
+                        }
+                        else
+                        {
+                            Baloons.Add(Baloons[i].Pop());
+                            Baloons.RemoveAt(i);
+                            Lasers.RemoveAt(j);
+                        }
+
                     }
                 }
  
