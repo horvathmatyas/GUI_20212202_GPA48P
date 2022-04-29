8dafb6312a0acb8ccc6e20ebdbff25cb2171cedc
zolijoo <joozoltan9@gmail.com>
2022-04-29 21:10:25 +0200

    created Display.cs and Renderer folder, added base methods to it: SetupSizes, SetupModel

---
+++ SpaceBaloons/Renderer/Display.cs
@@ -0,0 +1,31 @@
+﻿using SpaceBaloons.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace SpaceBaloons.Renderer
+{
+    internal class Display : FrameworkElement
+    {
+        Size area;
+        GameModel model;
+        public void SetupSizes(Size area)
+        {
+            this.area = area;
+            this.InvalidateVisual();
+        }
+        public void SetupModel(GameModel model)
+        {
+            this.model = model;
+            this.model.Changed += (sender, eventargs) => this.InvalidateVisual();
+        }
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+        }
+    }
+}
